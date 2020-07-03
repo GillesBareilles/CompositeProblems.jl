bf31bc4e5dd6d389a6a610b6663ce8f20cd8e776
Gilles Bareilles <gilles.bareilles@univ-grenoble-alpes.com>
2020-07-03 14:07:55 +0200

    --

--- src/CompositeProblems.jl
+++ src/CompositeProblems.jl
@@ -1,47 +1,55 @@
 module CompositeProblems
 
 using LinearAlgebra
+import StructuredProximalOperators: g, prox_αg!, prox_αg, ∇M_g!, ∇M_g, ∇²M_g_ξ!, ∇²M_g_ξ
 using StructuredProximalOperators
 using Random
 using Distributions
 
-# Write your package code here.
 
 abstract type CompositeProblem end
 
 
 
 ##
-problem_dimension(pb::CompositeProblem) = error("problem_dimension not implemented for a problem $(typeof(pb)).")
+function problem_dimension(pb::CompositeProblem)
+    return error("problem_dimension not implemented for a problem $(typeof(pb)).")
+end
 
 
 ## Smooth side
 # 0th order
-f(pb::CompositeProblem, x) = error("f not implemented for a problem $(typeof(pb)) and point $(typeof(x)).")
+function f(pb::CompositeProblem, x)
+    return error("f not implemented for a problem $(typeof(pb)) and point $(typeof(x)).")
+end
 
 # 1st order
 function ∇f!(pb::CompositeProblem, res, x)
-    error("∇f! not implemented for a problem $(typeof(pb)), result $(typeof(res)) and point $(typeof(x)).")
+    return error("∇f! not implemented for a problem $(typeof(pb)), result $(typeof(res)) and point $(typeof(x)).")
 end
 function ∇f(pb::CompositeProblem, x)
-    error("∇f not implemented for a problem $(typeof(pb)) and point $(typeof(x)).")
+    return error("∇f not implemented for a problem $(typeof(pb)) and point $(typeof(x)).")
 end
 
 # 2nd order
 function ∇²f_h!(pb::CompositeProblem, res, x, h)
-    error("∇²f_h! not implemented for a problem $(typeof(pb)), result $(typeof(res)), point $(typeof(x)) and direction $(typeof(h)).")
+    return error("∇²f_h! not implemented for a problem $(typeof(pb)), result $(typeof(res)), point $(typeof(x)) and direction $(typeof(h)).")
 end
 function ∇²f_h(pb::CompositeProblem, x, h)
-    error("∇²f_h not implemented for a problem $(typeof(pb)), point $(typeof(x)) and direction $(typeof(h)).")
+    return error("∇²f_h not implemented for a problem $(typeof(pb)), point $(typeof(x)) and direction $(typeof(h)).")
 end
 
 # conditioning
-get_gradlips(pb::CompositeProblem) = error("get_gradlips not implemented for a problem $(typeof(pb)).")
-get_μ_cvx(pb::CompositeProblem) = error("get_μ_cvx not implemented for a problem $(typeof(pb)).")
+function get_gradlips(pb::CompositeProblem)
+    return error("get_gradlips not implemented for a problem $(typeof(pb)).")
+end
+function get_μ_cvx(pb::CompositeProblem)
+    return error("get_μ_cvx not implemented for a problem $(typeof(pb)).")
+end
 
 
 ## Nonsmooth side
-g(pb::CompositeProblem, x) = (pb.regularizer)(x)
+g(pb::CompositeProblem, x) = g(pb.regularizer, x)
 
 prox_αg!(pb::CompositeProblem, res, x, α) = prox_αg!(pb.regularizer, res, x, α)
 prox_αg(pb::CompositeProblem, x, α) = prox_αg(pb.regularizer, x, α)
@@ -53,6 +61,7 @@ prox_αg(pb::CompositeProblem, x, α) = prox_αg(pb.regularizer, x, α)
 ∇²M_g_ξ!(pb::CompositeProblem, M, res, x, ξ) = ∇²M_g_ξ!(pb.regularizer, M, res, x, ξ)
 
 
+export CompositeProblem
 export f, ∇f!, ∇f, ∇²f_h!, ∇²f_h
 export get_gradlips, get_μ_cvx
 export g, prox_αg!, prox_αg, ∇M_g!, ∇M_g, ∇²M_g_ξ!, ∇²M_g_ξ
